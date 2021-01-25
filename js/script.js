8e35319e28bcb125c3af4b9759b3c44d1f466e2e
marco sartori <marcosartori98@yahoo.com>
2021-01-25 15:33:35 +0100

    add img e text

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus: Aggiungere un’immagine presa anch’essa da un data assieme al valore alt dell'attributo. Poi aggiungete all'immagine una classe sempre presa dal data.
 
-alert();
+var app = new Vue({
+    el: "#prova",
+    data: {
+        message: 'Vue è bellissimo',
+        src: 'https://vuejsdevelopers.com/images/posts/generic_vue_header.jpg',
+        titolo: 'vue'
+    }
+});
+
 
 
 
